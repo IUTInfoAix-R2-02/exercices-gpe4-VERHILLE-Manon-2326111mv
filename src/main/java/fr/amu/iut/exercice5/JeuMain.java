7911bafdcd7f0459f2f6cfb6d5e0768811907e3b
Manon VERHILLE <manon.verhille@etu.univ-amu.fr>
2024-04-17 09:57:40 +0200

    Exercice 5 terminé

--- src/main/java/fr/amu/iut/exercice5/JeuMain.java
+++ src/main/java/fr/amu/iut/exercice5/JeuMain.java
@@ -1,6 +1,7 @@
 package fr.amu.iut.exercice5;
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
@@ -9,13 +10,15 @@ import javafx.stage.Stage;
 
 import java.awt.*;
 
+import static java.lang.Thread.sleep;
+
 public class JeuMain extends Application {
 
     private Scene scene;
     private BorderPane root;
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws InterruptedException {
 
         root = new BorderPane();
 
@@ -24,6 +27,7 @@ public class JeuMain extends Application {
         Personnage fantome = new Fantome();
 
         // Création des obstacles
+        Obstacle mur = new Obstacle(300, 80, 40, 320);
 
         // on positionne le fantôme 20 positions vers la droite
         fantome.setLayoutX(20 * 10);
@@ -32,12 +36,16 @@ public class JeuMain extends Application {
         jeu.setPrefSize(640, 480);
         jeu.getChildren().add(pacman);
         jeu.getChildren().add(fantome);
+        jeu.getChildren().add(mur);
         root.setCenter(jeu);
         //on construit une scene 640 * 480 pixels
         scene = new Scene(root);
 
+        //Gestion du temps
+        timer();
+
         //Gestion du déplacement du personnage
-        deplacer(pacman, fantome);
+        deplacer(pacman, fantome, mur);
 
         primaryStage.setTitle("... Pac Man ...");
 
@@ -52,7 +60,7 @@ public class JeuMain extends Application {
      * @param j1
      * @param j2
      */
-    private void deplacer(Personnage j1, Personnage j2) {
+    private void deplacer(Personnage j1, Personnage j2, Obstacle mur) {
         scene.setOnKeyPressed((KeyEvent event) -> {
             switch (event.getCode()) {
                 case LEFT:
@@ -80,6 +88,47 @@ public class JeuMain extends Application {
                     j2.deplacerADroite(scene.getWidth());
                     break;
             }
+
+            // Collision J1 et mur
+            if(mur.getX() <= j1.getLayoutX()
+                    && j1.getLayoutX() <= (mur.getX() + 20)
+                    && j1.getLayoutY() == mur.getY()){
+                j1.deplacerEnHaut();
+            } else if(mur.getX() <= j1.getLayoutX()
+                    && j1.getLayoutX() <= (mur.getX() + 20)
+                    && j1.getLayoutY() == (mur.getY() + 300)) {
+                j1.deplacerEnBas(scene.getHeight());
+            } else if (mur.getY() <= j1.getLayoutY()
+                    && j1.getLayoutY() <= (mur.getY() + 300)
+                    && j1.getLayoutX() == mur.getX()) {
+                j1.deplacerAGauche();
+            } else if (mur.getY() <= j1.getLayoutY()
+                    && j1.getLayoutY() <= (mur.getY() + 300)
+                    && j1.getLayoutX() == (mur.getX() + 20)) {
+                j1.deplacerADroite(scene.getWidth());
+            }
+
+            System.out.print(j2.getLayoutX() + "\n" + j2.getLayoutY() + "\n" );
+            // Collision J2 et mur
+            if(mur.getX() <= j2.getLayoutX()
+                    && j2.getLayoutX() <= (mur.getX() + 20)
+                    && j2.getLayoutY() == mur.getY()){
+                j2.deplacerEnHaut();
+            } else if(mur.getX() <= j2.getLayoutX()
+                    && j2.getLayoutX() <= (mur.getX() + 20)
+                    && j2.getLayoutY() == (mur.getY() + 300)) {
+                j2.deplacerEnBas(scene.getHeight());
+            } else if (mur.getY() <= j2.getLayoutY()
+                    && j2.getLayoutY() <= (mur.getY() + 300)
+                    && j2.getLayoutX() == mur.getX()) {
+                j2.deplacerAGauche();
+            } else if (mur.getY() <= j2.getLayoutY()
+                    && j2.getLayoutY() <= (mur.getY() + 300)
+                    && j2.getLayoutX() == (mur.getX() + 20)) {
+                j2.deplacerADroite(scene.getWidth());
+            }
+
+            // Collision J1 et J2
             if (j1.estEnCollision(j2)){
                 System.out.println("Collision....");
                 System.exit(0);
@@ -87,5 +136,24 @@ public class JeuMain extends Application {
         });
     }
 
-
+    public void timer() throws InterruptedException {
+        final Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    final int max = 5;
+                    final long sleepTime = 1000;
+                    int value = max;
+                    while (value > 0) {
+                        sleep(sleepTime);
+                        value --;
+                    }
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+        t.start();
+    }
 }
+
--- src/main/java/fr/amu/iut/exercice5/Obstacle.java
+++ src/main/java/fr/amu/iut/exercice5/Obstacle.java
@@ -1,7 +1,12 @@
 package fr.amu.iut.exercice5;
 
+import javafx.scene.shape.Rectangle;
+
 import java.awt.*;
 
 public class Obstacle extends Rectangle {
 
+    public Obstacle(int x, int y, int width, int height){
+        super(x, y, width, height);
+    }
 }
