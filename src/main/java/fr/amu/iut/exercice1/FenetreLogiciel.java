2d739541699a69d544f7a3c4dfd87288fbcb3821
Manon VERHILLE <145963847+VERHILLE-Manon-2326111mv@users.noreply.github.com>
2024-05-07 21:39:59 +0200

    TP 1 terminé

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,13 +1,127 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class FenetreLogiciel extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // code de l'exercice 1
+        // Création d'un conteneur principal
+        BorderPane borderPane = new BorderPane();
+
+        // Création du conteneur correspondant à la ligne de contrôle au dessus du tableau
+        Menu menu1 = new Menu("File");
+        Menu menu2 = new Menu("Edit");
+        Menu menu3 = new Menu("Help");
+
+        MenuItem file1 = new MenuItem("New");
+        MenuItem file2 = new MenuItem("Open");
+        MenuItem file3 = new MenuItem("Save");
+        MenuItem file4 = new MenuItem("Close");
+
+        MenuItem edit1 = new MenuItem("Cut");
+        MenuItem edit2 = new MenuItem("Copy");
+        MenuItem edit3 = new MenuItem("Paste");
+
+        menu1.getItems().addAll(file1, file2, file3, file4);
+        menu2.getItems().addAll(edit1, edit2, edit3);
+
+        MenuBar menuBar = new MenuBar(menu1, menu2, menu3);
+
+        borderPane.setTop(menuBar);
+
+        // Création de la zone Boutons
+        VBox boutons = new VBox();
+        boutons.setAlignment(Pos.CENTER);
+        Label l1 = new Label("Boutons :");
+        Button bouton1 = new Button("Bouton 1");
+        Button bouton2 = new Button("Bouton 2");
+        Button bouton3 = new Button("Bouton 3");
+
+        boutons.getChildren().addAll(l1, bouton1, bouton2, bouton3);
+        boutons.setSpacing(10.0);
+
+        borderPane.setLeft(boutons);
+
+        // Conteneur du milieu
+        HBox milieu = new HBox();
+
+        // Création de la ligne de séparation
+        Separator sep1 = new Separator();
+        sep1.setOrientation(Orientation.VERTICAL);
+
+        // Création du menu de connexion
+        VBox connexion = new VBox();
+        connexion.setAlignment(Pos.CENTER);
+        GridPane formulaire = new GridPane();
+        formulaire.setAlignment(Pos.CENTER);
+        formulaire.setHgap(10);
+        formulaire.setVgap(10);
+        Label name = new Label("Name");
+        TextField champ1 = new TextField();
+        Label email = new Label("Email");
+        TextField champ2 = new TextField();
+        Label password = new Label("Password");
+        TextField champ3 = new TextField();
+
+        formulaire.add(name, 0, 0);
+        formulaire.add(champ1, 1, 0);
+        formulaire.add(email, 0, 1);
+        formulaire.add(champ2, 1, 1);
+        formulaire.add(password, 0, 2);
+        formulaire.add(champ3, 1, 2);
+
+        HBox boutons2 = new HBox();
+        boutons2.setAlignment(Pos.CENTER);
+        Button submit = new Button("Submit");
+        Button cancel = new Button("Cancel");
+
+        boutons2.getChildren().addAll(submit, cancel);
+        boutons2.setSpacing(10.0);
+
+        connexion.getChildren().addAll(formulaire, boutons2);
+
+        connexion.setSpacing(10.0);
+
+        milieu.getChildren().addAll(sep1, connexion);
+        milieu.setSpacing(250.0);
+
+        borderPane.setCenter(milieu);
+
+        // Conteneur bas
+        VBox basPage = new VBox();
+        basPage.setAlignment(Pos.CENTER);
+
+        // Création de la ligne de séparation
+        Separator sep2 = new Separator();
+        sep2.setOrientation(Orientation.HORIZONTAL);
+
+        // Création du bandeau en bas de la fenêtre
+        Label text = new Label("Ceci est un label de bas de page");
+
+        basPage.getChildren().addAll(sep2, text);
+
+        borderPane.setBottom(basPage);
+
+        // Ajout du conteneur à la scene
+        Scene scene = new Scene(borderPane);
+
+        // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
+        primaryStage.setScene( scene );
+        primaryStage.setWidth( 800 );
+        primaryStage.setHeight( 600 );
+        primaryStage.setTitle("Premier exemple manipulant les conteneurs");
+
+        // Affichage de la fenêtre
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/fr/amu/iut/exercice2/TicTacToe.java
+++ src/main/java/fr/amu/iut/exercice2/TicTacToe.java
@@ -1,12 +1,133 @@
 package fr.amu.iut.exercice2;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Random;
 
 public class TicTacToe extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        // Conteneur principal
+        GridPane grid = new GridPane();
+
+        // Création labels
+        Label cercle1 = new Label();
+        Label cercle2 = new Label();
+        Label cercle3 = new Label();
+        Label cercle4 = new Label();
+        Label croix1 = new Label();
+        Label croix2 = new Label();
+        Label croix3 = new Label();
+        Label vide1 = new Label();
+        Label vide2 = new Label();
+
+        ImageView imgCercle1 = new ImageView("exercice2/Rond.png");
+        cercle1.setGraphic(imgCercle1);
+        ImageView imgCercle2 = new ImageView("exercice2/Rond.png");
+        cercle2.setGraphic(imgCercle2);
+        ImageView imgCercle3 = new ImageView("exercice2/Rond.png");
+        cercle3.setGraphic(imgCercle3);
+        ImageView imgCercle4 = new ImageView("exercice2/Rond.png");
+        cercle4.setGraphic(imgCercle4);
+
+        ImageView imgCroix1 = new ImageView("exercice2/Croix.png");
+        croix1.setGraphic(imgCroix1);
+        ImageView imgCroix2 = new ImageView("exercice2/Croix.png");
+        croix2.setGraphic(imgCroix2);
+        ImageView imgCroix3 = new ImageView("exercice2/Croix.png");
+        croix3.setGraphic(imgCroix3);
+
+        ImageView imgVide1 = new ImageView("exercice2/Vide.png");
+        vide1.setGraphic(imgVide1);
+        ImageView imgVide2 = new ImageView("exercice2/Vide.png");
+        vide2.setGraphic(imgVide2);
+
+        // Ajout des éléments dans la scène
+        int x = 0, y = 0, nbCercle = 4, nbCroix = 3, nbVide = 2;
+        for(int i = 0; i < 9; ++i) {
+            Random random = new Random();
+
+            int nombre = random.nextInt(3);
+            if ((nombre == 0 && nbCercle == 0) || (nombre == 1 && nbCroix == 0) || (nombre == 2 && nbVide == 0)) {
+                --i;
+                if(x == 0){
+                    x = 2;
+                    --y;
+                } else {
+                    --x;
+                }
+            }
+
+            if (nombre == 0) {
+                if (nbCercle == 4) {
+                    grid.add(cercle1, x, y);
+                } else if (nbCercle == 3) {
+                    grid.add(cercle2, x, y);
+                } else if (nbCercle == 2) {
+                    grid.add(cercle3, x, y);
+                } else if (nbCercle == 1) {
+                    grid.add(cercle4, x, y);
+                }
+                --nbCercle;
+            } else if (nombre == 1) {
+                if (nbCroix == 3) {
+                    grid.add(croix1, x, y);
+                } else if (nbCroix == 2) {
+                    grid.add(croix2, x, y);
+                } else if (nbCroix == 1) {
+                    grid.add(croix3, x, y);
+                }
+                --nbCroix;
+            } else {
+                if (nbVide == 2) {
+                    grid.add(vide1, x, y);
+                } else if (nbVide == 1) {
+                    grid.add(vide2, x, y);
+                }
+                --nbVide;
+            }
+
+            if (x == 2) {
+                x = 0;
+                ++y;
+            } else {
+                ++x;
+            }
+        }
+
+        grid.setGridLinesVisible(true);
+
+        /*grid.addRow(0 , cercle1 , croix1, croix2);
+        grid.addRow(1 , cercle2 , cercle3, cercle4);
+        grid.addRow(2 , vide1 , vide2, croix3);
+        */
+
+        /*grid.add(cercle1, 0, 0);
+        grid.add(croix1, 1, 0);
+        grid.add(croix2, 2, 0);
+        grid.add(cercle2, 0, 1);
+        grid.add(cercle3, 1, 1);
+        grid.add(cercle4, 2, 1);
+        grid.add(vide1, 0, 2);
+        grid.add(vide2, 1, 2);
+        grid.add(croix3, 2, 2);*/
+
+        // Scène
+        Scene scene = new Scene(grid);
+
+        // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
+        primaryStage.setScene( scene );
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Tic Tac Toe");
+
         primaryStage.show();
     }
 }
