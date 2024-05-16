17687d1ca9cc48895c1298da2020a47ee9f6ee10
Manon VERHILLE <manon.verhille@etu.univ-amu.fr>
2024-05-16 12:09:15 +0200

    Exemple partie3 finie

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,15 +1,27 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
 
+    @FXML
+    private Button decrementButton;
+    @FXML
+    private Button incrementButton;
+    @FXML
+    private Label counterLabel = new Label("0");
     int counter = 0;
 
     public void increment() {
+
     }
 
     public void decrement() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
-open module tp.intro.javafx {
+module tp.intro.javafx {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
     requires java.desktop;
+
+    opens fr.amu.iut.com.example.partie3 to javafx.fxml;
+    exports fr.amu.iut.com.example.partie3;
 }
